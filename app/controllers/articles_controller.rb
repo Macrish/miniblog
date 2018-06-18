e042c420a304b0d1e632995f5c2a2d4e813e89e1
Macrish <tatianadavydiuk@gmail.com>
2018-06-18 23:54:31 +0300

    basic authenticate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "name", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "name", password: "secret", only: :desrtoy
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
